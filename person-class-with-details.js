88eb6b9acfc0043d135a7ab00f9e4e71f6044e96
SaleamlakH <saleamlak.7h@gmail.com>
2026-01-22 21:31:27 +0300

    Feat: Add static method which compare two persons and return the older

--- person-class-with-details.js
+++ person-class-with-details.js
@@ -2,6 +2,8 @@
  * create a class called "Person" with properties for name, age and country. 
  * a method to display the person's details. 
  * Create two instances  and display their details.
+ * 
+ * a static method to compare the ages of two Person instances and returns the older one.
  */
 
 class Person {
@@ -14,6 +16,10 @@ class Person {
     details() {
         return `Name: ${this.name}, Age: ${this.age}, Country: ${this.country}`;
     }
+
+    static whoIsOlder(person1, person2) {
+        return person1.age > person2.age ? person1 : person2;
+    }
 }
 
 let person1 = new Person('Rekik', '24', 'Ethiopia');
@@ -24,3 +30,6 @@ console.log(person1.details());
 
 console.log('\nperson2 details');
 console.log(person2.details());
+
+let olderPerson = Person.whoIsOlder(person1, person2);
+console.log("\nThe older person is\n", olderPerson.details());
